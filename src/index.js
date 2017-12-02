b46aa28de82612433e98340dd3c91e872902185d
Eric Hansen <hansen.c.eric@gmail.com>
2017-12-02 16:19:04 -0500

    Add in Status support

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ let apiKey = '';
 let apiBase = '';
 let matches = require('./matches');
 let players = require('./players');
+let status = require('./status');
 
 module.exports.init = function (brApiKey, brApiBase) {
   apiKey = brApiKey;
   apiBase = brApiBase;
 
+  // Available routes
   matches.init(brApiKey, brApiBase);
+  status.init(brApiKey, brApiBase);
+
+  // Current WIP routes (either due to me or BattleRite API devs)
   players.init(brApiKey, brApiBase);
 
   this.matches = matches;
+  this.status = status;
 }
 
 /**
  * Supported exposed SDK methods
  */
 module.exports.matches = matches;
+module.exports.status = status;
--- test/index.test.js
+++ test/index.test.js
@@ -7,30 +7,58 @@ var assert = require('chai').assert;
 
 describe('index:', function () {
 
-  it('should provide matches object', function (done) {
+  describe('matches:', function () {
+    it('should provide matches object', function (done) {
     
-    should.exist(entrypoint);
-    expect(entrypoint).to.have.property('matches');
+      should.exist(entrypoint);
+      expect(entrypoint).to.have.property('matches');
 
-    assert.isFunction(entrypoint.matches.getMatchesBasic);
-    assert.isFunction(entrypoint.matches.getMatchesDetailed);
-    assert.isFunction(entrypoint.matches.getMatchBasic);
-    assert.isFunction(entrypoint.matches.getMatchDetailed);
+      assert.isFunction(entrypoint.matches.getMatchesBasic);
+      assert.isFunction(entrypoint.matches.getMatchesDetailed);
+      assert.isFunction(entrypoint.matches.getMatchBasic);
+      assert.isFunction(entrypoint.matches.getMatchDetailed);
 
-    done();
-  });
-
-  describe('properties can make calls', function () {
-    before(function () {
-      entrypoint.init(process.env.BR_API_KEY);
+      done();
     });
 
-    it.only('should return data from matches object', function () {
+    describe('properties can make calls', function () {
+      before(function () {
+        entrypoint.init(process.env.BR_API_KEY);
+      });
 
-      return entrypoint.matches.getMatchesBasic().then(function (matchData) {
-        should.exist(matchData);
-        expect(matchData).to.have.property('data');
+      it('should return data from matches object', function () {
+
+        return entrypoint.matches.getMatchesBasic().then(function (matchData) {
+          should.exist(matchData);
+          expect(matchData).to.have.property('data');
+        });
       });
     });
-  });  
+  });
+  
+  describe('status:', function () {
+    it('should provide status object', function (done) {
+
+      should.exist(entrypoint);
+      expect(entrypoint).to.have.property('status');
+
+      assert.isFunction(entrypoint.status.getStatus);
+
+      done();
+    });
+
+    describe('properties can make calls', function () {
+      before(function () {
+        entrypoint.init(process.env.BR_API_KEY);
+      });
+
+      it('should return data from status object', function () {
+
+        return entrypoint.status.getStatus().then(function (apiStatus) {
+          should.exist(apiStatus);
+          expect(apiStatus).to.have.property('data');
+        });
+      });
+    });
+  });
 });
